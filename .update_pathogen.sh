ddab62a073ee5642e9fbc4ba38d1c6e796604385
Tantas <joseph.preiss@gmail.com>
2013-05-14 22:43:24 -0400

    Adding update pathogen script to update and install vim plugins.

---
+++ .update_pathogen.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Vim configuration directories
+autoloadDirectory="~/.vim/autoload"
+vimbundleDirectory="~/.vim/bundle"
+
+# Update vim-pathogen
+mkdir -p "${autoloadDirectory}" "${vimbundleDirectory}"
+curl -Sso "${autoloadDirectory}/pathogen.vim" \
+    "https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim"
+
+# Update the bundles
+vimbundles=(
+	"git://github.com/tpope/vim-sensible" \
+	"git://github.com/scrooloose/nerdtree" \
+	"git://github.com/tpope/vim-markdown" \
+
+)
+cd "${vimbundleDirectory}"
+for vimbundlerepository in ${vimbundles[@]}; do
+	echo "Checking bundle ${vimbundlerepository}"
+	repositoryname=$(basename ${vimbundlerepository})
+	if [[ -d ${repositoryname} ]]; then
+		cd ${repositoryname}
+		git pull
+		cd ../
+	else
+		echo 'Cloning bundle '
+		git clone -q ${vimbundlerepository}
+	fi
+done
+
+# Delete bundles by removing from vimbundles and "${vimbundleDirectory}"
+
